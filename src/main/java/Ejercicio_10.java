3fd447c7bd69893ea33403e7b0795cb06370fcdf
jsepucas <jsepucas@myuax.com>
2024-02-07 17:16:09 +0100

    ultimo

--- src/main/java/Ejercicio_10.java
+++ src/main/java/Ejercicio_10.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Ejercicio10 {
+public class Ejercicio_10 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
