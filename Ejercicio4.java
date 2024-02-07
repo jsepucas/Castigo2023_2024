85464f4b8b46260bbd45f664280116029e7b6237
jsepucas <jsepucas@myuax.com>
2024-02-07 14:33:33 +0100

    5

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ejercicio4
+public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numero;
@@ -28,3 +28,14 @@ public class Ejercicio4
                 scanner.next(); // Limpiar el buffer del scanner
             }
         }
+
+        if (min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) {
+            System.out.println("No se introdujeron números positivos.");
+        } else {
+            System.out.println("El mínimo es: " + min);
+            System.out.println("El máximo es: " + max);
+        }
+
+        scanner.close();
+    }
+}
